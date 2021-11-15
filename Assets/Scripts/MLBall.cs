5d1de6fb347b59b74f2387d78a46f0c437c12228
David <andedavi@oregonstate.edu>
2021-11-15 14:11:50 -0800

    Add minimum velocity, update start ball speed to be a bit slower, update ball start x coordinate to be more manageable

--- Assets/Scripts/MLBall.cs
+++ Assets/Scripts/MLBall.cs
@@ -32,6 +32,17 @@ public class MLBall : MonoBehaviour
             Tuple<float, float> ballposition = generateBallPosition();
             transform.position = new Vector3(ballposition.Item1, ballposition.Item2);
             LaunchBall();
+        } else {
+            int yValue = 5;
+            if (rigidBody.velocity.y > -5 && rigidBody.velocity.y < 5) {
+                if (rigidBody.velocity.y <= 0) {
+                    yValue = -5;
+                } else if (rigidBody.velocity.y > 0) {
+                    yValue = 5;
+                }
+                Vector2 minimumVelocity = new Vector2(0, yValue);
+                rigidBody.AddForce(minimumVelocity);
+            }
         }
         previousVelocity = rigidBody.velocity;
     }
@@ -39,16 +50,16 @@ public class MLBall : MonoBehaviour
     private void LaunchBall()
     {
         Renderer visual = GetComponent<Renderer>();
-        inPlay = true;
         float x = UnityEngine.Random.Range(0, 2) == 0 ? -1 : 1;
-        Vector2 direction = new Vector2((float)UnityEngine.Random.Range(-12, 12), -12);
-        visual.enabled = true;
+        Vector2 direction = new Vector2((float)UnityEngine.Random.Range(-12, 12), -10);
         rigidBody.AddForce(direction);
+        inPlay = true;
+        visual.enabled = true;
     }
 
     private Tuple<float, float> generateBallPosition()
     {
-        randomXCoord = UnityEngine.Random.Range(1f, 9f);
+        randomXCoord = UnityEngine.Random.Range(5f, 10f);
         randomYCoord = UnityEngine.Random.Range(3.25f, 3.75f);
         return new Tuple<float, float>(randomXCoord, randomYCoord);
     }
